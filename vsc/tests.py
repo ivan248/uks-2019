c13f764d89ebf0794e564b7a35822fcc05af71a8
ivan248 <ivan248959@gmail.com>
2019-03-19 16:07:49 +0100

    Configuring Travis...

--- vsc/tests.py
+++ vsc/tests.py
@@ -4,4 +4,15 @@ from __future__ import unicode_literals
 from django.test import TestCase
 
 # Create your tests here.
+def fun(x):
+    return x + 1
+
+class MyTest(TestCase):
+
+    def test_dummy(self):
+        self.assertEqual(fun(3), 4)
+
+
+
+
 
