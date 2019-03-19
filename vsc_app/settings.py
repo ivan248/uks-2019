946f2699dcb7f5bb4383a4afd70ac81b9ebb3c92
ivan248 <ivan248959@gmail.com>
2019-03-19 14:07:40 +0100

    Added vsc app to django project structure. Added 2 test models (Question and answer).

--- vsc_app/settings.py
+++ vsc_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'vsc.apps.VscConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
