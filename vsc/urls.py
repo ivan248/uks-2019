85e2958f1df82ef4da3bd45229f76299ed75857e
filipetrovic <fpetrovic@ymail.com>
2019-03-27 19:02:13 +0100

    (feat) Issue, Milestone, Label, Assignee
    Added CRUD for entities Issue, Milestone, Label, Assignee.
    Lists and views for these entities and relationships between them

--- vsc/urls.py
+++ vsc/urls.py
@@ -5,24 +5,40 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login$', views.login, name='login'),
     url(r'^register$', views.register, name='register'),
+    #PROJECT
     url(r'^projects$', views.project_list, name='project_list'),
     url(r'^projects/(?P<project_id>[0-9]+)$', views.project_detail, name="project_detail"),
     url(r'^projects/create$', views.project_create, name="project_create"),
     url(r'^projects/(?P<project_id>[0-9]+)/update', views.project_update, name="project_update"),
     url(r'^projects/(?P<project_id>[0-9]+)/delete', views.project_delete, name="project_delete"),
     url(r'^projects/(?P<project_id>[0-9]+)/add_member', views.project_add_member, name="project_add_member"),
+    #BRANCH
     url(r'^projects/(?P<project_id>[0-9]+)/branches/(?P<branch_id>[0-9]+)$', views.branch_detail, name="branch_detail"),
     url(r'^projects/(?P<project_id>[0-9]+)/branches$', views.branch_create, name="branch_create"),
     url(r'^projects/(?P<project_id>[0-9]+)/branches/(?P<branch_id>[0-9]+)/update', views.branch_update, name="branch_update"),
     url(r'^projects/(?P<project_id>[0-9]+)/branches/(?P<branch_id>[0-9]+)/delete', views.branch_delete, name="branch_delete"),
+    #COMMIT
     url(r'^projects/(?P<project_id>[0-9]+)/branches/(?P<branch_id>[0-9]+)/commits/(?P<commit_id>[0-9]+)$', views.commit_detail, name="commit_detail"),
     url(r'^projects/(?P<project_id>[0-9]+)/branches/(?P<branch_id>[0-9]+)/commits$', views.commit_create, name="commit_create"),
     url(r'^projects/(?P<project_id>[0-9]+)/branches/(?P<branch_id>[0-9]+)/commits/(?P<commit_id>[0-9]+)/update$', views.commit_update, name="commit_update"),
     url(r'^projects/(?P<project_id>[0-9]+)/branches/(?P<branch_id>[0-9]+)/commits/(?P<commit_id>[0-9]+)/delete$', views.commit_delete, name="commit_delete"),
+    #ISSUE
     url(r'^projects/(?P<project_id>[0-9]+)/issues$', views.issue_create, name="issue_create"),
     url(r'^projects/(?P<project_id>[0-9]+)/issues/(?P<issue_id>[0-9]+)$', views.issue_detail, name="issue_detail"),
     url(r'^projects/(?P<project_id>[0-9]+)/issues/(?P<issue_id>[0-9]+)/update$', views.issue_update, name="issue_update"),
     url(r'^projects/(?P<project_id>[0-9]+)/issues/(?P<issue_id>[0-9]+)/delete$', views.issue_delete, name="issue_delete"),
+    url(r'^projects/(?P<project_id>[0-9]+)/issues/(?P<issue_id>[0-9]+)/add_label', views.issue_add_label, name="issue_add_label"),
+    url(r'^projects/(?P<project_id>[0-9]+)/issues/(?P<issue_id>[0-9]+)/add_assignee', views.issue_add_assignee, name="issue_add_assignee"),
+    #MILESTONE
     url(r'^projects/(?P<project_id>[0-9]+)/issues/(?P<issue_id>[0-9]+)/milestones$', views.milestone_create, name="milestone_create"),
+    url(r'^projects/(?P<project_id>[0-9]+)/issues/(?P<issue_id>[0-9]+)/milestones/(?P<milestone_id>[0-9]+)$', views.milestone_detail, name="milestone_detail"),
+    url(r'^projects/(?P<project_id>[0-9]+)/issues/(?P<issue_id>[0-9]+)/milestones/(?P<milestone_id>[0-9]+)/update$', views.milestone_update, name="milestone_update"),
+    url(r'^projects/(?P<project_id>[0-9]+)/issues/(?P<issue_id>[0-9]+)/milestones/(?P<milestone_id>[0-9]+)/delete$', views.milestone_delete, name="milestone_delete"),
+    #LABEL
+    url(r'^labels/create$', views.label_create, name="label_create"),
+    url(r'^labels$', views.label_list, name="label_list"),
+    url(r'^labels/(?P<label_id>[0-9]+)$', views.label_detail, name="label_detail"),
+    url(r'^labels/(?P<label_id>[0-9]+)/update$', views.label_update, name="label_update"),
+    url(r'^labels/(?P<label_id>[0-9]+)/delete$', views.label_delete, name="label_delete"),
 
 ]
--- vsc/views.py
+++ vsc/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.utils.crypto import get_random_string
 import datetime
 
-from .models import Project, ProjectMembership, Branch, Commit, User, CurrentUser, Issue, IssueAssignments, Milestone
+from .models import Project, ProjectMembership, Branch, Commit, User, CurrentUser, Issue, IssueAssignments, Milestone, Label, IssueLabels
 
 
 def index(request):
@@ -210,12 +210,25 @@ def issue_detail(request, project_id, issue_id):
     issue = Issue.objects.get(id=issue_id)
     template = loader.get_template('vsc/issue_detail.html')
     milestones = Milestone.objects.filter(issue=issue)
-    # labels = Commit.objects.filter(branch=branch)
-    # assignees
+
+    issue_labels = IssueLabels.objects.filter(issue=issue)
+    labels = []
+
+    for issue_label in issue_labels:
+        labels.append(issue_label.label)
+
+    issue_assignments = IssueAssignments.objects.filter(issue=issue)
+    assignees = []
+
+    for issue_assignment in issue_assignments:
+        assignees.append(issue_assignment.user)
+
     context = {
         'project': project,
         'issue': issue,
-        'milestones': milestones
+        'milestones': milestones,
+        'labels': labels,
+        'assignees': assignees
     }
     return HttpResponse(template.render(context, request))
 
@@ -245,6 +258,35 @@ def issue_delete(request, project_id, issue_id):
         Issue.objects.filter(id=issue_id).delete()
         return HttpResponseRedirect(reverse('vsc:project_detail',kwargs={'project_id':project_id}))
 
+def issue_add_label(request, project_id, issue_id):
+    if request.method == 'POST':
+        label_name = request.POST['label_name']
+
+        issue = get_object_or_404(Issue, id=issue_id)
+        label = get_object_or_404(Label, name=label_name)
+
+        found = IssueLabels.objects.filter(issue=issue.id, label=label.id).count()
+        if found != 0:
+            return HttpResponse("This label is already added")
+
+        issue_label = IssueLabels(issue=issue, label=label)
+        issue_label.save()
+        return HttpResponseRedirect(reverse('vsc:issue_detail', kwargs={'project_id':project_id, 'issue_id': issue_id}))
+
+def issue_add_assignee(request, project_id, issue_id):
+    if request.method == 'POST':
+        assignee_email = request.POST['assignee_email']
+        issue = get_object_or_404(Issue, id=issue_id)
+
+        assignee = get_object_or_404(User, email=assignee_email)
+
+        found = IssueAssignments.objects.filter(issue=issue.id, user=assignee.id).count()
+        if found != 0:
+            return HttpResponse("This assignee is already added")
+
+        assignment = IssueAssignments(issue=issue, user=assignee)
+        assignment.save()
+        return HttpResponseRedirect(reverse('vsc:issue_detail', kwargs={'project_id':project_id, 'issue_id': issue_id}))
 
 def milestone_create(request, project_id, issue_id):
     milestone_name = request.POST['milestone_name']
@@ -260,3 +302,92 @@ def milestone_create(request, project_id, issue_id):
     return HttpResponseRedirect(
         reverse('vsc:issue_detail', kwargs={'project_id': project_id, 'issue_id': issue_id})
     )
+
+def milestone_detail(request, project_id, issue_id, milestone_id):
+
+    project = Project.objects.get(id=project_id)
+    issue = Issue.objects.get(id=issue_id)
+    milestone = Milestone.objects.get(id=milestone_id)
+
+    template = loader.get_template('vsc/milestone_detail.html')
+    context = {
+        'project': project,
+        'issue': issue,
+        'milestone': milestone
+    }
+    return HttpResponse(template.render(context, request))
+
+def milestone_update(request, project_id, issue_id, milestone_id):
+
+    if request.method == 'POST':
+        milestone_name = request.POST['milestone_name']
+        milestone_description = request.POST['milestone_description']
+        milestone = Milestone.objects.get(id=milestone_id)
+        milestone.name = milestone_name
+        milestone.description = milestone_description
+        milestone.save()
+        return HttpResponseRedirect(
+            reverse('vsc:issue_detail', kwargs={'project_id':project_id, 'issue_id': issue_id})
+        )
+
+def milestone_delete(request, project_id, issue_id, milestone_id):
+
+    if request.method == 'POST':
+        Milestone.objects.filter(id=milestone_id).delete()
+        return HttpResponseRedirect(
+            reverse('vsc:issue_detail',kwargs={'project_id':project_id, 'issue_id': issue_id})
+        )
+
+def label_create(request):
+    label_name = request.POST['label_name']
+    label_description = request.POST['label_description']
+    label_color = request.POST['label_color']
+    label = Label(
+        name=label_name,
+        description=label_description,
+        color=label_color
+    )
+    label.save()
+    return HttpResponseRedirect(reverse('vsc:label_list'))
+
+def label_list(request):
+    labels = Label.objects.all()
+    template = loader.get_template('vsc/label_list.html')
+    context = {
+        'labels': labels,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def label_detail(request, label_id):
+    label = Label.objects.get(id=label_id)
+    template = loader.get_template('vsc/label_detail.html')
+
+    context = {
+        'label': label,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def label_update(request, label_id):
+
+    if request.method == 'POST':
+        label_name = request.POST['label_name']
+        label_description = request.POST['label_description']
+        label_color = request.POST['label_color']
+
+        label = Label.objects.get(id=label_id)
+
+        label.name = label_name
+        label.description = label_description
+        label.color = label_color
+        label.save()
+        return HttpResponseRedirect(reverse('vsc:label_list'))
+
+def label_delete(request, label_id):
+
+    if request.method == 'POST':
+        Label.objects.filter(id=label_id).delete()
+        return HttpResponseRedirect(reverse('vsc:label_list'))
+
+
