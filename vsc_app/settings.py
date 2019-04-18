d97922b005e75d734f8d78223b13599eea54e067
ivan248 <ivan248959@gmail.com>
2019-04-18 10:27:24 +0200

    Switched to Postres database.

--- vsc_app/settings.py
+++ vsc_app/settings.py
@@ -82,22 +82,22 @@ WSGI_APPLICATION = 'vsc_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql',
-#          'NAME': 'postgres',
-#          'USER': 'postgres',
-#          'HOST': 'db',
-#          'PORT': 5432,
-#      }
-#  }
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': 'mydatabase',
-   }
-}
+      'default': {
+          'ENGINE': 'django.db.backends.postgresql',
+          'NAME': 'postgres',
+          'USER': 'postgres',
+          'HOST': 'db',
+          'PORT': 5432,
+      }
+  }
+
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': 'mydatabase',
+#   }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
